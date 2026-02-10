dcab54b56ebf7790b208629b3b8cd82a31f053f5
Ybg <pdool177@gmail.com>
2026-02-09 22:13:29 -0800

    Initial Commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Roblox_Settings_Modifier;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
